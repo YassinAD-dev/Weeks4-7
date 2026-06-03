3a61467235665598f41c41772b911a0bd1f87954
Yassin Abdelnader <abdelnay@shernet.sheridancollege.ca>
2026-06-03 15:12:11 -0400

    added the ability to move with using the keyboard input (WASD)

---
+++ Assets/Scripts/Alien hover.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class Alienhover : MonoBehaviour
+{
+    SpriteRenderer Alien;
+    public Button itsYou;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    } 
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //Alien.bound;
+
+
+
+        ///if (Mouse.current.position) {
+
+    }
+}
