2e0f16acbd6da3aab3a389c4adfaee75ca520c6b
Yassin Abdelnader <abdelnay@shernet.sheridancollege.ca>
2026-06-09 16:30:11 -0400

    manging the ui healthbard

--- Assets/Scripts/Healthbar.cs
+++ Assets/Scripts/Healthbar.cs
@@ -1,16 +1,23 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Healthbar : MonoBehaviour
 {
+    public Image healthbarFillImage;
+
+    public float currentHealth = 100f;
+    public float maxHealth = 100f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        healthbarFillImage.fillAmount = currentHealth / maxHealth;
     }
 }
+
